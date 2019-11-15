45e59860ade3584624378536fb6953c86875a81b
Submit2CVPR <57785794+Submit2CVPR@users.noreply.github.com>
2019-11-15 14:26:46 +0800

    Add files via upload

---
+++ DBGAN_link/run.py
@@ -0,0 +1,16 @@
+import settings
+
+from link_prediction import Link_pred_Runner
+from clustering import Clustering_Runner
+
+dataname = 'cora'       # 'cora' or 'citeseer' or 'pubmed'
+model = 'DBGAN'          # 'arga_ae' or 'DBGAN'
+task = 'link_prediction'    
+
+settings = settings.get_settings(dataname, model, task)
+
+if task == 'link_prediction':
+    runner = Link_pred_Runner(settings)
+
+runner.erun()
+
